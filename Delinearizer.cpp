ccda584fcba543bc58ca9cb61c350af0215e0a21
Tibor Áser Veres (RPBCACUEAIIBH) <v.tibor.a@gmail.com>
2021-01-26 01:23:43 +0200

    Delinearizer v2.0.0 update. Testing and examples required! Re-wrote it, now everything has -1 to 1 as well as 0 to 100 range. Added several other curves, mainly for artificial neurons.

--- Delinearizer.cpp
+++ Delinearizer.cpp
@@ -3,31 +3,143 @@
 
 /* Calculation */
 
-float Logarithmic (float Input)
+float Logarithmic (float Input, boolean Range)
 {
-  return 100 - 100 / 2 * log10(100 - Input * 99 / 100);
-}
-
-float InverseLogarithmic (float Input)
-{
-  return 100 / 2 * log10(Input * 99 / 100 + 1);
-}
-
-float SCurve (float Input)
-{
-  if (Input < 50)
-    return InverseLogarithmic (Input) / ScaleILL;
+  if (Range == false)
+  {
+    Input = 100.0 - Trim (Input) * 99.0 / 100.0;
+    return 100.0 - 50.0 * log10(Input);
+  }
   else
-    return Logarithmic (Input) / ScaleILL + OffsetILL;
-}
+  {
+    Input = 100.0 - Trim (Input + 1.0, 0.0, 2.0) * 99.0 / 1.99999;
+    return 1.0 - log10(Input);
+  }
+}// Execution time: 222.18us (Range: 0.0 to 100.0); 229.496us (Range: -1.0 to 1.0)
 
-float LearningCurve (float Input)
+float InverseLogarithmic (float Input, boolean Range)
 {
-  if (Input < 49.99999)
-    return 49.99144 - SCurve (50 - Input);
+  if (Range == false)
+  {
+    Input = Trim (Input) * 99.0 / 100.0 + 1.0;
+    return 50.0 * log10(Input);
+  }
   else
-    return SCurve (Input - 50) + 49.99144;
-}
+  {
+    Input = Trim (Input + 1.0, 0.0, 2.0) * 99.0 / 1.99999 + 1.0;
+    return log10(Input) - 1.0;
+  }
+}// Execution time: 223.02us (Range: 0.0 to 100.0); 230.292us (Range: -1.0 to 1.0)
+
+float SCurve (float Input, boolean Range)
+{
+  if (Range == false)
+  {
+    Input = Trim (Input);
+    if (Input < 50.0)
+      return InverseLogarithmic (Input) / ScaleILL;
+    else
+      return Logarithmic (Input) / ScaleILL + OffsetILL;
+  }
+  else
+  {
+    Input = Trim (Input + 1.0, 0.0, 2.0) * 50.0;
+    if (Input < 50.0)
+      return (InverseLogarithmic (Input) / ScaleILL) / 50 - 1;
+    else
+      return (Logarithmic (Input) / ScaleILL + OffsetILL) / 50 - 1;
+  }
+}// Execution time: 281.124us (Range: 0.0 to 100.0); 335.092us (Range: -1.0 to 1.0)
+
+float BellCurve (float Input, boolean Range)
+{
+  if (Range == false)
+  {
+    Input = Trim (Input);
+    return 50.0 + 50.0 * sin ((-90.0 + Input * (360.0 / 100.0)) * (PI / 180.0));
+  }
+  else
+  {
+    Input = Trim (Input + 1.0, 0.0, 2.0);
+    return sin ((-90.0 + Input * (360.0 / 2.0)) * (PI / 180.0));
+  }
+}// Execution time: 151.1us (Range: 0.0 to 100.0); 158.828us (Range: -1.0 to 1.0)
+
+float ReLU (float Input, boolean Range = 1)
+{
+  if (Range == true)
+    return Trim (Input, 0.0, 1.0);
+  else
+    return Trim (Input, 50.0, 100.0);
+}// Execution time: 16.496us (Range: 0.0 to 100.0); 16.568us (Range: -1.0 to 1.0)
+
+float SigmoidExp (float Input, boolean Range)
+{
+  if (Range == true)
+  {
+    Input = Trim (Input, -1.0, 1.0) * 10;
+    return (1.0 / (1.0 + exp (-Input))) * 2 - 1;
+  }
+  else
+  {
+    Input = Trim (Input);
+    return (1.0 / (1.0 + exp (-((Input - 50) / 5)))) * 100;
+  }
+}// Execution time: 218.224us (Range: 0.0 to 100.0); 190.088us (Range: -1.0 to 1.0)
+
+float SigmoidSine (float Input, boolean Range)
+{
+  if (Range == true)
+  {
+    Input = Trim (Input + 1, 0.0, 2.0);
+    return sin ((-90.0 + Input * (180.0 / 2.0)) * (PI / 180.0));
+  }
+  else
+  {
+    Input = Trim (Input);
+    return 50.0 + 50.0 * sin ((-90.0 + Input * (180.0 / 100.0)) * (PI / 180.0));
+  }
+}// Execution time: 148.748us (Range: 0.0 to 100.0); 156.468us (Range: -1.0 to 1.0)
+
+float SigmoidInvLog (float Input, boolean Range)
+{
+  if (Range == true)
+  {
+    Input = Trim (Input + 1.0, 0.0, 2.0) * 50.0;
+    if (Input < 49.99999)
+      return (50.00856 - InverseLogarithmic (50.0 - Input) / ScaleILL) / 50.0 - 1.0;
+    else
+      return (InverseLogarithmic (Input - 50.0) / ScaleILL + 49.99144) / 50.0 - 1.0;
+  }
+  else
+  {
+    Input = Trim (Input);
+    if (Input < 49.99999)
+      return 50.00856 - InverseLogarithmic (50.0 - Input) / ScaleILL;
+    else
+      return InverseLogarithmic (Input - 50.0) / ScaleILL + 49.99144;
+  }
+}// Execution time: 285.128us (Range: 0.0 to 100.0); 338.396us (Range: -1.0 to 1.0)
+
+
+
+
+
+/*float TestCurve (float Input, boolean Range)
+{
+  //Input = Trim (Input);
+  //return 50.0 + 50.0 * sin ((-90.0 + Input * (180.0 / 100.0)) * (PI / 180.0));
+  
+  if (Input < -5.0)
+    Input = -5.0;
+  else if (Input > 5.0)
+    Input = 5.0;
+  return sin ((Input * (180.0 / 10.0)) * (PI / 180.0)) / 2.0 + 0.5;
+}// Execution time: us (Range: 0.0 to 100.0); us (Range: -1.0 to 1.0) */
+
+
+
+
 
 /* Conversion */
 
@@ -43,4 +155,14 @@ unsigned int FtoI (float Input)
   if (Input > 100)
     Input = 100;
   return (unsigned int)(Input * 100);
+}
+
+float Trim (float Input, float LowerLimit, float UpperLimit)
+{
+  if (Input < LowerLimit)
+    return LowerLimit;
+  else if (Input > UpperLimit)
+    return UpperLimit;
+  else
+    return Input;
 }
--- Delinearizer.h
+++ Delinearizer.h
@@ -8,13 +8,30 @@
 #define ScaleILL 1.703
 #define OffsetILL 100-(100-100/2*log10(1))/ScaleILL
 
-float Logarithmic (float Input);
-float InverseLogarithmic (float Input);
-float SCurve (float Input);
-float LearningCurve (float Input); // Learning curve for artificial neurons.
+// These are meant for simulating non linear potmeter value.
+// Default range is from 0.0 to 100.0
+float Logarithmic (float Input, boolean Range = 0);
+float InverseLogarithmic (float Input, boolean Range = 0);
+float SCurve (float Input, boolean Range = 0);
 
-/* Conversion */
+// Not sure what this will be good for...
+// Default range is from 0.0 to 100.0
+float BellCurve (float Input, boolean Range = 0); // Just cause I can... :P
+
+// These are meant for artificial neurons.
+// Default range is from -1.0 to 1.0
+float ReLU (float Input, boolean Range = 1);
+float SigmoidExp (float Input, boolean Range = 1);
+float SigmoidSine (float Input, boolean Range = 1); // This is my invention...
+float SigmoidInvLog (float Input, boolean Range = 1); // Also my invention...
+
+// This is for testing...
+// Default range is from -1.0 to 1.0
+/*float TestCurve (float Input, boolean Range = 1);//*/
+
+/* Conversion and trimming */
 float ItoF (unsigned int Input);
 unsigned int FtoI (float Input);
+float Trim (float Input, float LowerLimit = 0.0, float UpperLimit = 100.0);
 
 #endif
